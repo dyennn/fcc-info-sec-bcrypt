cb71298c00952a34cdc6028484bc8df9f8971306
Dyen <deanielaquino7@gmail.com>
2025-07-28 22:55:56 +0800

    Tried using hash and comparing it with plain text

--- server.js
+++ server.js
@@ -11,8 +11,24 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  /*Store hash in your db*/
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(hash);
+    
+    // Now compare the password with the generated hash
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+      /*res == true or false*/
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(res);
+      }
+    });
+  }
+});
 
 //END_ASYNC
 
