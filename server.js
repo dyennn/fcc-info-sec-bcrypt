bf246f6262a529c473f91147043ac16da8ab2030
Dyen <deanielaquino7@gmail.com>
2025-07-28 23:06:58 +0800

    Used hash and compare methods

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
-    console.log(hash);
-    bcrypt.compare(someOtherPlaintextPassword, hash, (err, res) => {
-        console.log(res);
-    });
+  console.log(hash);
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res);
+  });
 });
 //END_ASYNC
 
